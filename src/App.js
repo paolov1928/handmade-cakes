53519be9c28aeb2eb49b14c9a10b0e8da6754a2c
kbberker <kbbuk90@gmail.com>
2019-05-23 15:55:52 +0100

    Changed component import name

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from "react"
-import "./App.css"
-import { BrowserRouter as Router, Route } from "react-router-dom"
-import Navbar from "./Components/Navbar"
-import Home from "./Components/Home"
-import About from "./Components/About"
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Route path="/" component={Home} />
+      <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
