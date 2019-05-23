af24fd61f274b760ab005ab67064a8d3cb7090f9
paolov1928 <ventura.paolo@gmail.com>
2019-05-23 15:53:55 +0100

    made the components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from "react"
+import "./App.css"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Navbar from "./Components/Navbar"
+import Home from "./Components/Home"
+import About from "./Components/About"
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Route path="/" component={ContentContainer} />
+      <Route path="/" component={Home} />
       <Route exact path="/about" component={About} />
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
