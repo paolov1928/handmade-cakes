bec16c9fd9ec67bacc19bdad5f44965469fcf1eb
kbberker <kbbuk90@gmail.com>
2019-05-23 15:43:51 +0100

    Add react router routes and dummy components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Navbar />
+      <Route path="/" component={ContentContainer} />
+      <Route exact path="/about" component={About} />
+    </Router>
   );
 }
 
