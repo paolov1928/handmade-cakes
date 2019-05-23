6edfdd4d11e0a2bfef57a68f21f995f8da5b6ca9
paolov1928 <ventura.paolo@gmail.com>
2019-05-23 16:17:07 +0100

    added navbar styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,16 @@
 import React from "react"
+import styled, { css } from "styled-components"
+
+const NavbarContainer = styled.div`
+  text-align: center;
+  background-color: black;
+  color: white;
+  height: 50px;
+  display: flex;
+`
 
 const Navbar = () => {
-  return <div>Nav</div>
+  return <NavbarContainer>a</NavbarContainer>
 }
 
 export default Navbar
