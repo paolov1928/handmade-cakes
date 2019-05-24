48188a1fd6e757706c0673f0e20e6d89ce7e672f
paolov1928 <ventura.paolo@gmail.com>
2019-05-24 10:45:06 +0100

    added a bit of text on the about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,14 @@
 import React from "react"
+import styled, { css } from "styled-components"
+
+const Title = styled.h3`
+  color: white;
+  margin: 1vh;
+  font-family: cursive;
+`
 
 const About = () => {
-  return <div>About</div>
+  return <Title>I am Effie the best baker in the whole of the 7 kingdoms</Title>
 }
 
 export default About
